6ce1e5fa573ba1f0100ddd0ffe521d0f0f001ad6
Lachlan Murphy <lachlan.g.murphy@gmail.com>
2022-12-22 19:20:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 let express = require('express');
 
+let port = 3000;
+
 let app = express();
-let server = app.listen(3000);
+let server = app.listen(port);
 
 app.use(express.static('public'));
+
+console.log("Server running on port " + port);
+
+console.log("New Shit");
